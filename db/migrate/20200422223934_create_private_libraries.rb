9e725072ccdea870f9a7e8eededfbf54418b5c78
aprilfaithdays <aprilfaith.e@gmail.com>
2020-04-22 20:04:03 -0400

    relationships added

--- db/migrate/20200422223934_create_private_libraries.rb
+++ db/migrate/20200422223934_create_private_libraries.rb
@@ -1,6 +1,6 @@
 class CreatePrivateLibraries < ActiveRecord::Migration[6.0]
   def change
-    create_table :private_libraries do |t|
+    create_table :personal_libraries do |t|
       t.references :user, null: false, foreign_key: true
       t.references :workout, null: false, foreign_key: true
 
