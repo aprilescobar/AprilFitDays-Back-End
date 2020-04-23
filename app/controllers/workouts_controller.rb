eac0a7516a33a00b47a4e5d865801d3b2dcaeb5e
aprilfaithdays <aprilfaith.e@gmail.com>
2020-04-22 20:29:13 -0400

    controllers set up

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,33 @@
 class WorkoutsController < ApplicationController
+    def index
+        workouts = Workout.all
+        render json: workouts, except: [:created_at, :updated_at]
+    end
+
+    def show
+        workout = Workout.find(params[:id])
+        render json: workout, except: [:created_at, :updated_at]
+    end
+
+    def create
+        workout = Workout.create(workout_params)
+        render json: workout, except: [:created_at, :updated_at], status: 201
+    end
+
+    def update
+        workout = Workout.find_by(id: params[:id])
+        workout.update(workout_params)
+        render json: workout, except: [:created_at, :updated_at], status: 201
+    end
+
+    def destroy
+        workout = Workout.find_by(id: params[:id])
+        workout.destroy
+    end
+
+    private
+
+    def workout_params
+        params.require(:workout).permit(:user_id, :name, :description, :media)
+    end
 end
