54e3c5490a2d6eec6adf92f89bf4fb4d0133f4fc
aprilfaithdays <aprilfaith.e@gmail.com>
2020-04-28 18:28:11 -0400

    added user model in personal_libraries controler for workout model

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,23 +1,23 @@
 class WorkoutsController < ApplicationController
     def index
         workouts = Workout.all
-        render json: workouts, except: [:created_at, :updated_at]
+        render json: workouts, except: [:created_at, :updated_at], include: [:user]
     end
 
     def show
         workout = Workout.find(params[:id])
-        render json: workout, except: [:created_at, :updated_at]
+        render json: workout, except: [:created_at, :updated_at], include: [:user]
     end
 
     def create
         workout = Workout.create(workout_params)
-        render json: workout, except: [:created_at, :updated_at], status: 201
+        render json: workout, except: [:created_at, :updated_at],include: [:user], status: 201
     end
 
     def update
         workout = Workout.find_by(id: params[:id])
         workout.update(workout_params)
-        render json: workout, except: [:created_at, :updated_at], status: 201
+        render json: workout, except: [:created_at, :updated_at], include: [:user], status: 201
     end
 
     def destroy
